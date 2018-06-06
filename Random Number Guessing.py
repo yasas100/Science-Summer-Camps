694dae2802d3324ae19c38999929928252ba0ecd
Yasas Witharana <yasas@ualberta.ca>
2018-06-06 11:24:46 -0600

    Test!

--- Random Number Guessing.py
+++ Random Number Guessing.py
@@ -4,7 +4,7 @@ while (playagain):
     randomNumber=random.randint(1,10)
     usrGuess= int(input("Guess a number between 1 and 10 \n"))
     if(usrGuess==randomNumber):
-        print("Congratulations your guess was correct!")
+        print("Congratulations your guess was correct! ")
     else:
         print("Incorrect Guess, the number was "+ str(randomNumber) )
         #playagain1= input("Would you like to play again (y/n)").lower
