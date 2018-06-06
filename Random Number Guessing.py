be768c8adfe9836dbd100f24ff56cc4b14296ed2
Cody Steinke <wit056@cs.ualberta.ca>
2018-06-06 11:15:57 -0600

    Added loop for user to play again

--- Random Number Guessing.py
+++ Random Number Guessing.py
@@ -1,8 +1,15 @@
 import random
-randomNumber=random.randint(1,10)
-usrGuess= int(input("Guess a number between 1 and 10 \n"))
-if(usrGuess==randomNumber):
-    print("Congratulations your guess was correct!")
-else:
-    print("Incorrect Guess, the number was "+ str(randomNumber) + " ,please play again!")
-                
+playagain=True
+while (playagain):
+    randomNumber=random.randint(1,10)
+    usrGuess= int(input("Guess a number between 1 and 10 \n"))
+    if(usrGuess==randomNumber):
+        print("Congratulations your guess was correct!")
+    else:
+        print("Incorrect Guess, the number was "+ str(randomNumber) )
+        #playagain1= input("Would you like to play again (y/n)").lower
+    if((input("Would you like to play again (y/n)")).lower()=="y"):
+        playagain=True
+    else:
+        playagain=False
+                    
